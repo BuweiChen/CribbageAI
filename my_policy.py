a6659fa79a4bb3e62bec6ccc3bc26cd9ebd794ec
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-01-29 22:50:43 -0500

    Implement basic keep function, curr score: 0.169
    Current keep policy: for each possible keep, consider all turn cards
    and add together scores of hand and scores of crib (only considering
    the 2 player discards and the turn). The keep with max score is kept.
    
    Current peg policy: default

--- my_policy.py
+++ my_policy.py
@@ -1,4 +1,6 @@
 from policy import CribbagePolicy, CompositePolicy, GreedyThrower, GreedyPegger
+import itertools
+import scoring
 
 class MyPolicy(CribbagePolicy):
     def __init__(self, game):
@@ -8,14 +10,44 @@ class MyPolicy(CribbagePolicy):
         deck = self._policy._game.deck()
         deck.remove(hand)
         return deck.peek(deck.size())
+    
+    def _get_comb_of_any_four(self, hand):
+        """ returns a list of possible kept hand and throws
+        """
+        all_combinations = []
+        for combination in itertools.combinations(hand, 4):
+            remaining_cards = tuple(set(hand) - set(combination))
+            all_combinations.append((combination, remaining_cards))
+        
+        return all_combinations
+    
+    def _get_value_of_crib_discard_pair(self, cards, turn, am_dealer):
+        return scoring.score(self._policy._game, cards, turn, True)[0] if am_dealer else -scoring.score(self._policy._game, cards, turn, True)[0]
         
     def keep(self, hand, scores, am_dealer):
         """ basic keep policy: considers turn card and greedy opponent discard
         """
         
-        possibleTurnCard = self._get_possible_turn_cards(hand)
+        # print("new hand! ~~~~~")
         
-        return self._policy.keep(hand, scores, am_dealer)
+        possibleTurnCard = self._get_possible_turn_cards(hand)
+        possibleKeepThrow = self._get_comb_of_any_four(hand)
+        
+        bestKeepThrow = ()
+        bestKeepThrowScore = float('-inf')
+        for comb in possibleKeepThrow:
+            score = 0
+            for card in possibleTurnCard:
+                score += scoring.score(self._policy._game, comb[0], card, False)[0] + self._get_value_of_crib_discard_pair(comb[1], card, am_dealer)
+                # print("turn: {0} card: {1} score: {2}".format(card, comb[0], score))
+            if score > bestKeepThrowScore:
+                    bestKeepThrow = comb
+                    bestKeepThrowScore = score
+            
+        
+        # print("best keep is {0} with score {1}".format(list(bestKeepThrow[1]), bestKeepThrowScore))
+                    
+        return list(bestKeepThrow[0]), list(bestKeepThrow[1])
 
 
     def peg(self, cards, history, turn, scores, am_dealer):
